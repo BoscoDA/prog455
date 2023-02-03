286b1ed4157f6b993171eadd8970fe96c318f7b7
BoscoDA <nickwe02@gmail.com>
2023-02-03 15:22:40 -0600

    Adds homework assignments for week 2

---
+++ Homework/Week2-Service/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week2_Service
+{
+    public partial class Form1 : Form
+    {
+        
+        MessageService messageService = new MessageService();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+
+            var num1 = tb_number1.Text;
+            var num2 = tb_number2.Text;
+
+            var newMessage = messageService.ReturnCalculationMessage(num1, num2);
+
+            MessageBox.Show(newMessage);
+        }
+    }
+}
---
+++ Homework/Week2-Service/MathService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Service
+{
+    internal class MathService
+    {
+        public double ReturnAddition(double num1, double num2)
+        {
+
+            return num1 + num2;
+        }
+
+        public double ReturnSubtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double ReturnMultiplication(double num1, double num2)
+        {
+            var product = num1 * num2;
+            return product;
+        }
+
+        public double ReturnDivision(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
