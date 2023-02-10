5d98076d4b06170f7d351736cdc4b23f6ee93d79
BoscoDA <nickwe02@gmail.com>
2023-02-10 14:00:30 -0600

    Added week 3 assignment with database functionality

---
+++ Homework/Week3Databases/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week3Databases.MvcApplication" Language="C#" %>
---
+++ Homework/Week3Databases/Services/Service.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week3Databases.Services
+{
+    public class Service
+    {
+    }
+}
