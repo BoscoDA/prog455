7b1cbe94ee68e14c4c24e2d83d02f0fcdb65c563
BoscoDA <nickwe02@gmail.com>
2023-02-18 14:07:28 -0600

    Added sql scripts for the generation of the DB used for this class and the tables used in the week 3 & 4 HW

---
+++ DB Scripts/Character_Table.sql
@@ -0,0 +1,28 @@
+USE [PROG455SP23]
+GO
+
+/****** Object:  Table [dbo].[Character]    Script Date: 2/18/2023 2:05:58 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Character](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nchar](100) NOT NULL,
+	[Element_Type] [nchar](50) NOT NULL,
+	[Job_Class] [nchar](100) NOT NULL,
+	[ATK] [int] NOT NULL,
+	[DEF] [int] NOT NULL,
+	[Mana] [int] NOT NULL,
+	[HP_Initial] [int] NOT NULL,
+	[HP_Max] [int] NOT NULL,
+	[NPC] [bit] NOT NULL,
+ CONSTRAINT [PK_Character] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
