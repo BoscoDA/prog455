4cbc4b786d2c7800416b2e4476017e94751a3f26
BoscoDA <nickwe02@gmail.com>
2023-03-22 17:53:10 -0500

    implements a exception handler on the main method

--- Homework/Week8AssignmentDesignPatterns/Program.cs
+++ Homework/Week8AssignmentDesignPatterns/Program.cs
@@ -11,8 +11,18 @@ namespace Week8AssignmentDesignPatterns
     {
         static void Main(string[] args)
         {
+            System.AppDomain.CurrentDomain.UnhandledException += UnhandledExecptionTrapper;
             Game game = new Game();
             game.Start();
+        }
+
+        //Exception handler taken from here: https://stackoverflow.com/questions/3133199/net-global-exception-handler-in-console-application
+        static void UnhandledExecptionTrapper(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine(e.ExceptionObject.ToString());
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+            Environment.Exit(1);
         }
     }
 }
