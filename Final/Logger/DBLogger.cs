77cc1bc0ba6fba43c1c83f0d3011d913b97790b1
BoscoDA <nickwe02@gmail.com>
2023-05-04 13:53:58 -0500

    Added project for DB logger

---
+++ Final/Logger/DBLogger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Code taken from example provided in PROG 455 Spring 2023
+ * All code was based off of code created by Leo Hazou
+ */
+namespace Logger
+{
+    internal class DBLogger : ILogger
+    {
+        private string _sqlConnString;
+        public void DbLogger()
+        {
+            //typically would be own singleton class so you don't have to build it all the time
+            PrepareConnection();
+
+        }
+        public void LogError(string message, Exception ex)
+        {
+            string query = $"Insert into dbo.Log (LogLevel,LogMessage,Exception)" +
+                $"VALUES( '{LoggerLevel.ErrorLevel.Error}','{message}','{ex}')";
+
+            using (SqlConnection conn = new SqlConnection(_sqlConnString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(query, conn);
+                conn.Open();
+
+                sqlCommand.ExecuteNonQuery();
+
+                conn.Close();
+            }
+        }
+
+        public void LogInfo(string message)
+        {
+            string query = $"Insert into dbo.Log (LogLevel,LogMessage)" +
+                $"VALUES( '{LoggerLevel.ErrorLevel.Information}','{message}')";
+
+            using (SqlConnection conn = new SqlConnection(_sqlConnString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(query, conn);
+                conn.Open();
+
+                sqlCommand.ExecuteNonQuery();
+
+                conn.Close();
+            }
+        }
+
+        public void LogWarning(string message)
+        {
+            string query = $"Insert into dbo.Log (LogLevel,LogMessage)" +
+                $"VALUES( '{LoggerLevel.ErrorLevel.Warning}','{message}')";
+
+            using (SqlConnection conn = new SqlConnection(_sqlConnString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(query, conn);
+                conn.Open();
+
+                sqlCommand.ExecuteNonQuery();
+
+                conn.Close();
+            }
+        }
+
+        private void PrepareConnection()
+        {
+            //string immutable, will build over and over again when appended
+            //string builder holds values and then when you are done you get the string
+            SqlConnectionStringBuilder connBldr = new SqlConnectionStringBuilder();
+            connBldr.DataSource = $"(localdb)\\MSSQLLocalDB";
+            connBldr.IntegratedSecurity = true;
+            connBldr.InitialCatalog = $"GuessThatPokemon";
+            _sqlConnString = connBldr.ConnectionString;
+        }
+    }
+}
---
+++ Final/Logger/LoggerLevel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Code taken from example provided in PROG 455 Spring 2023
+ * All code created by Leo Hazou
+ */
+namespace Logger
+{
+    internal class LoggerLevel
+    {
+        public sealed class ErrorLevel
+        {
+            public const string Error = "Error";
+            public const string Warning = "Warning";
+            //compile time (favor this)
+            public const string Information = "Info";
+            // difference between public const string and static readonly string is public const string will not communicate well between different assemblies, static readonly string will
+
+            //run time
+            //static readonly string Test = "";
+        }
+    }
+}
